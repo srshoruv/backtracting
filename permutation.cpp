e85d7e6a6c33fa568169db251ba91d7682e09b07
srshoruv <srshoruv2002@gmail.com>
2024-09-03 02:36:45 +0600

    permutation

---
+++ permutation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void permutation(string str, string ans) {
+    int n = str.size();
+    if (n==0) {
+        cout << ans << endl;
+        return;
+    }
+
+    for (int i=0; i<n; i++) {
+        char ch = str[i];
+        string nextStr = str.substr(0,i) + str.substr(i+1, n-1-i);
+        permutation(nextStr, ans+ch);
+    }
+}
+
+int main(){
+    string str = "abc";
+    string ans = "";
+    permutation(str, ans);
+}
